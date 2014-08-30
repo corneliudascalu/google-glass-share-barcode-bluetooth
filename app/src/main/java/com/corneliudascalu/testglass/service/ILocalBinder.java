08349fcf07def662c3a3152695a689b64c922f5e
Corneliu Dascalu <corneliu.dascalu@gmail.com>
2014-08-30 23:04:40 +0300

    Created interfaces for bluetooth services

---
+++ app/src/main/java/com/corneliudascalu/testglass/service/ILocalBinder.java
@@ -0,0 +1,9 @@
+package com.corneliudascalu.testglass.service;
+
+/**
+ * @author Corneliu Dascalu <corneliu.dascalu@gmail.com>
+ */
+public interface ILocalBinder {
+
+    BluetoothInterface getService();
+}
