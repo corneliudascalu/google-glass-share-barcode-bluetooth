5657f7e7af5474df554e926f95dc57adfc5ba816
Corneliu Dascalu <corneliu.dascalu@gmail.com>
2014-09-02 22:25:23 +0300

    Better design, scrolling log

--- phone/src/main/java/com/corneliudascalu/glass/phone/MainActivity.java
+++ phone/src/main/java/com/corneliudascalu/glass/phone/MainActivity.java
@@ -1,8 +1,14 @@
 package com.corneliudascalu.glass.phone;
 
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormatter;
+import org.joda.time.format.DateTimeFormatterBuilder;
+
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.text.method.ScrollingMovementMethod;
 import android.util.Pair;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -18,15 +24,33 @@ public class MainActivity extends ActionBarActivity {
     @InjectView(R.id.text)
     TextView textView;
 
+    private DateTimeFormatter formatter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EventBus.getDefault().register(this);
         setContentView(R.layout.activity_main);
         ButterKnife.inject(this, this);
+        DateTimeFormatterBuilder builder = new DateTimeFormatterBuilder();
+        formatter = builder.appendHourOfDay(2).appendLiteral(':')
+                .appendMinuteOfHour(2).appendLiteral(':')
+                .appendSecondOfMinute(2).toFormatter();
         startService(new Intent(this, BluetoothService.class));
+
+        textView.setMovementMethod(new ScrollingMovementMethod());
+        textView.setTypeface(Typeface.MONOSPACE);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -41,19 +65,25 @@ public class MainActivity extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_clear) {
+            textView.setText("");
             return true;
         }
         return super.onOptionsItemSelected(item);
     }
 
     public void onEventMainThread(Pair<Integer, String> pair) {
+        DateTime dateTime = new DateTime();
         switch (pair.first) {
             case BluetoothService.MSG_CONNECTED:
-                textView.append("\n" + pair.second);
+                //textView.setText(dateTime.toString(formatter) + " - "+ pair.second + "\n" + textView.getText());
+                textView.append(dateTime.toString(formatter) + " - "
+                        + pair.second + "\n");
                 break;
             case BluetoothService.MSG_READ_DATA:
-                textView.append("\n" + pair.second);
+                //textView.setText(dateTime.toString(formatter) + " - "+ pair.second + "\n" + textView.getText());
+                textView.append(dateTime.toString(formatter) + " - "
+                        + pair.second + "\n");
                 break;
 
         }
