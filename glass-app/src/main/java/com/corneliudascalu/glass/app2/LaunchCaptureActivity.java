7fae71b042a18010655258deed048384a60e06a2
Corneliu Dascalu <corneliu.dascalu@gmail.com>
2014-09-03 22:06:58 +0300

    Fixed voice triggers

--- glass-app/src/main/java/com/corneliudascalu/glass/app2/LaunchCaptureActivity.java
+++ glass-app/src/main/java/com/corneliudascalu/glass/app2/LaunchCaptureActivity.java
@@ -1,9 +1,12 @@
 package com.corneliudascalu.glass.app2;
 
+import com.google.android.glass.app.Card;
+
 import com.github.barcodeeye.scan.CaptureActivity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 /**
  * @author Corneliu Dascalu <corneliu.dascalu@gmail.com>
@@ -13,7 +16,15 @@ public class LaunchCaptureActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(buildView());
         startActivity(CaptureActivity.newIntent(this));
+    }
+
+    private View buildView() {
+        Card card = new Card(this);
+
+        card.setText("Launching capture...");
+        return card.getView();
+
     }
 }
