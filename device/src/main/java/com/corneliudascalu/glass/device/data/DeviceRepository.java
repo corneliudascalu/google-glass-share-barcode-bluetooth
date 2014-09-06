5e4372a916cd3c3f522ad47b300cfaa5dfd6bf93
Corneliu Dascalu <corneliu.dascalu@gmail.com>
2014-09-06 18:01:53 +0300

    Fixed get devices request

--- device/src/main/java/com/corneliudascalu/glass/device/data/DeviceRepository.java
+++ device/src/main/java/com/corneliudascalu/glass/device/data/DeviceRepository.java
@@ -2,9 +2,9 @@ package com.corneliudascalu.glass.device.data;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.reflect.TypeToken;
 
 import com.corneliudascalu.glass.device.model.Device;
+import com.corneliudascalu.glass.device.model.DeviceList;
 import com.corneliudascalu.glass.device.model.DeviceMessage;
 import com.squareup.okhttp.MediaType;
 import com.squareup.okhttp.OkHttpClient;
@@ -28,11 +28,18 @@ public class DeviceRepository {
         Response response = client.newCall(request).execute();
         String json = response.body().string();
 
-        // json= "[{\"name\":\"asd\",\"token\":\"asdasd\"},{\"name\":\"asd\",\"token\":\"asdasd\"},{\"name\":\"asd\",\"token\":\"asdasd\"}]";
+        /**
+         * {"Devices":
+         * [{"token":"9480ace500012eb931f847fde2c5508c6c9769871d7c9324132dba67ba4dc9d0","name":"Sergiu Grigoriev's iPhone","ostype":1},
+         * {"token":"APA91bEsmnxK1qnv4jUfoGs3HssMTgM4lMuNJcTOY4p9bjCRMUlot5ODI_lIiGbO5N6K5TYZtpl02kTrf6OQYFAIr8oL1YNP8a0gTKILbmAeYY6KELnP5lDHdYzGJpibfYoi_uYULoa4P79pcrhmJQGioDXtUTt54C0yjOMbAG9wjSQbYWYpFDo","name":"corneliu.dascalu@gmail.com","ostype":2},
+         * {"token":"asdasdasd","name":"Vasiles ipadddddd","ostype":1},
+         * {"token":"dfaerq34rfvrae4tq3asdfasdf","name":"Cornelius Nexus 5","ostype":2}]}
+         */
         Gson gson = new GsonBuilder().create();
-        List<Device> devices = gson.fromJson(json, new TypeToken<List<Device>>() {
-        }.getType());
-        return devices;
+        /*List<Device> devices = gson.fromJson(json, new TypeToken<List<Device>>() {
+        }.getType());*/
+        DeviceList devices = gson.fromJson(json, DeviceList.class);
+        return devices.getDevices();
     }
 
     public boolean sendData(Device device, String data) throws IOException {
---
+++ device/src/main/java/com/corneliudascalu/glass/device/model/DeviceList.java
@@ -0,0 +1,20 @@
+package com.corneliudascalu.glass.device.model;
+
+import java.util.ArrayList;
+
+/**
+ * @author Corneliu Dascalu <corneliu.dascalu@gmail.com>
+ */
+public class DeviceList {
+
+    private ArrayList<Device> Devices;
+
+    public DeviceList() {
+
+    }
+
+
+    public ArrayList<Device> getDevices() {
+        return Devices;
+    }
+}
